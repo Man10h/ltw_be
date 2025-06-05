eff5a7f7d89346ed79d72e06331281a26aa9412e
B22DCCN521 Nguyễn Đức Mạnh <153792822+Man10h@users.noreply.github.com>
2025-06-05 11:16:23 +0000

    a

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -3,19 +3,19 @@ const mongoose = require("mongoose");
 const User = require("../db/userModel");
 const router = express.Router();
 const verifyToken = require("../middleware/auth");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const secretKey = "ltw_spring_2025";
 
-router.get("/list", verifyToken, async function(req, res){
+router.get("/list", async function (req, res) {
   try {
     const users = await User.find();
     return res.json(users);
   } catch (error) {
     console.log(error);
   }
-})
+});
 
-router.get("/:userId", verifyToken, async function(req, res){
+router.get("/:userId", verifyToken, async function (req, res) {
   const userId = req.params.userId;
   try {
     const user = await User.findById(userId);
@@ -26,17 +26,32 @@ router.get("/:userId", verifyToken, async function(req, res){
   } catch (error) {
     console.log(error);
   }
-})
+});
 
-router.post("/user", verifyToken, async function(req, res){
-  const {login_name, password, repassword, first_name, last_name, location, description, occupation} = req.body;
-  if(!login_name || !password || !first_name || !last_name || repassword !== password){
-    return res.status(400).json({message: "Bad Request"});
+router.post("/user", async function (req, res) {
+  const {
+    login_name,
+    password,
+    repassword,
+    first_name,
+    last_name,
+    location,
+    description,
+    occupation,
+  } = req.body;
+  if (
+    !login_name ||
+    !password ||
+    !first_name ||
+    !last_name ||
+    repassword !== password
+  ) {
+    return res.status(400).json({ message: "Bad Request" });
   }
   try {
-    const userExists = await User.findOne({login_name: login_name});
-    if(userExists){
-      return res.status(400).json({message: "Bad Request"});
+    const userExists = await User.findOne({ login_name: login_name });
+    if (userExists) {
+      return res.status(400).json({ message: "Bad Request" });
     }
     const user = new User({
       login_name: login_name,
@@ -45,24 +60,22 @@ router.post("/user", verifyToken, async function(req, res){
       last_name: last_name,
       location: location,
       description: description,
-      occupation: occupation
-    })
+      occupation: occupation,
+    });
     await user.save();
 
     const user_id = user._id;
     jwt.sign({ user_id }, secretKey, { expiresIn: "1h" }, (err, token) => {
       if (err) {
-        res.status(400).json({message: "Bad Request"});
+        res.status(400).json({ message: "Bad Request" });
       } else {
-        res.json({ token: token, id: user_id, login_name: login_name});
-    }
+        res.json({ token: token, id: user_id, login_name: login_name });
+      }
     });
-
-
   } catch (error) {
     console.log(error);
-    return res.status(400).json({message: "Bad Request"});
+    return res.status(400).json({ message: "Bad Request" });
   }
-})
+});
 
 module.exports = router;
